7f0133768282cb403ea6b97e4d27ce03d9b4ff9a
Susaid8 <sumayasaid15@icloud.com>
2022-04-28 14:02:55 -0500

    done

--- server/index.js
+++ server/index.js
@@ -1,45 +1,51 @@
 const express = require('express')
 const path = require('path')
-
-const app = express()
-app.use(express.json())
+const Rollbar = require('rollbar')
 
 // include and initialize the rollbar library with your access token
-var Rollbar = require('rollbar')
-var rollbar = new Rollbar({
-  accessToken: '42faef5956d04c1fb821f153417c8ff9',
-  captureUncaught: true,
-  captureUnhandledRejections: true,
-})
 
-// record a generic message and send it to Rollbar
-rollbar.log('Hello world!')
+let rollbar = new Rollbar({
+    accessToken: '42faef5956d04c1fb821f153417c8ff9',
+    captureUncaught: true,
+    captureUnhandledRejections: true,
+  })
+  
+  // record a generic message and send it to Rollbar
+  rollbar.log('Hello world!')
+
+const app = express()
+
+app.use(express.json())
+app.use('/style', express.static('./styles.css'))
+
+
 
 let student = []
 
-app.post('/api/student', (req, res)=>{
-    let {name} = req.body
-    name = name.trim()
-
-    students.push(name)
-
-    res.status(200).send(students)
-})
-
-app.post('/api/student', (req, res)=>{
-    let {name} = req.body
-    name = name.trim()
-
-    students.push(name)
-
-    rollbar.log('Student added successfully', {author: 'Sumaya', type: 'manual entry'})
-
-    res.status(200).send(students)
-})
-
 app.get('/', (req,res) => {
     res.sendFile(path.join(__dirname, '../index.html'))
+    rollbar.info('html file served correctly')
 })
+
+app.post('/api/student', (req, res)=>{
+    let {name} = req.body
+    name = name.trim()
+
+    const index = students.findIndex(studentName=> studentName === name)
+
+    if(index === -1 && name !== ''){
+        students.push(name)
+        rollbar.log('Student added successfully', {author: 'Sumaya', type: 'manual entry'})
+        res.status(200).send(students)
+    } else if (name === ''){
+        rollbar.error('No name given')
+        res.status(400).send('must provide a name.')  
+    } else {
+        rollbar.error('student already exists')
+        res.status(400).send('that student already exists')
+    }
+})
+
 
 const port = process.env.PORT || 4545
 
