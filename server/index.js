9754e6758dd3f8c26573d400b256a6ff9c64d086
Susaid8 <sumayasaid15@icloud.com>
2022-04-28 13:50:55 -0500

    looking for error

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const path = require('path')
 
 const app = express()
+app.use(express.json())
 
 // include and initialize the rollbar library with your access token
 var Rollbar = require('rollbar')
@@ -14,10 +15,35 @@ var rollbar = new Rollbar({
 // record a generic message and send it to Rollbar
 rollbar.log('Hello world!')
 
+let student = []
+
+app.post('/api/student', (req, res)=>{
+    let {name} = req.body
+    name = name.trim()
+
+    students.push(name)
+
+    res.status(200).send(students)
+})
+
+app.post('/api/student', (req, res)=>{
+    let {name} = req.body
+    name = name.trim()
+
+    students.push(name)
+
+    rollbar.log('Student added successfully', {author: 'Sumaya', type: 'manual entry'})
+
+    res.status(200).send(students)
+})
+
 app.get('/', (req,res) => {
     res.sendFile(path.join(__dirname, '../index.html'))
 })
 
 const port = process.env.PORT || 4545
 
+app.use(rollbar.errorHandler())
+
 app.listen(port, () => console.log(`Running on ${port}.`))
+
